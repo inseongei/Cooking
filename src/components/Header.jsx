bd729158d6d99b78a7b23325475ab048a082335c
INSEONG-PC\정인성 <twin2688@naver.com>
2023-12-19 16:45:09 +0900

    헤더 반응형 처리완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Logo from "../asset/Logo.png";
 import { BsDoorClosed } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
+  const [menu, setMenu] = useState(false);
   return (
-    <Container>
-      <div className="logo">
-        <img src={Logo} alt="로고" className="logo-img" />
-      </div>
-      <ul className="Menu-list">
-        <li>레시피 광장</li>
-        <li>쿠킹 클래스</li>
-        <li>쿡 쇼핑</li>
-      </ul>
-      <div className="login-box">
-        <BsDoorClosed className="login-btn" />
+    <Container menu={menu.toString()}>
+      <div className="header-container">
+        <div className="logo">
+          <img src={Logo} alt="로고" className="logo-img" />
+        </div>
+        <ul className="Menu-list">
+          <li>레시피 광장</li>
+          <li>쿠킹 클래스</li>
+          <li>쿡 쇼핑</li>
+          <li className="menu-login">로그인</li>
+        </ul>
+        <div className="login-box">
+          <BsDoorClosed className="login-btn" />
+        </div>
+        <GiHamburgerMenu className="Menu-icon" onClick={() => setMenu(!menu)} />
       </div>
     </Container>
   );
 }
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   width: 100%;
   height: 8vh;
-
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
   .logo {
     display: flex;
     align-items: center;
@@ -68,5 +75,66 @@ const Container = styled.div`
   .login-btn:hover {
     cursor: pointer;
     color: #ffd700;
+  }
+
+  .Menu-icon {
+    display: none;
+    font-size: 1.5rem;
+    position: absolute;
+    right: 2%;
+    top: 3%;
+    cursor: pointer;
+  }
+
+  .menu-login {
+    display: none;
+  }
+
+  @media screen and (max-width: 768px) {
+    .header-container {
+      align-items: flex-start;
+      flex-direction: column;
+    }
+    .Menu-list {
+      display: ${(props) => (props.menu === "true" ? "flex" : "none")};
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      transition: all 0.2s ease 0s;
+      z-index: 1;
+      background-color: #fff;
+    }
+
+    .Menu-list li:nth-child(1) {
+    }
+
+    .Menu-list li {
+      width: 100%;
+      text-align: center;
+      border-bottom: 1px solid rgb(229, 229, 229);
+    }
+
+    .login-box {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      display: none;
+    }
+
+    .login-btn {
+      display: ${(props) => (props.menu === "true" ? "block" : "none")};
+    }
+
+    .Menu-icon {
+      display: block;
+    }
+
+    .menu-login {
+      display: block;
+    }
+
+    .Menu-list li:hover {
+      background-color: #fff;
+    }
   }
 `;
