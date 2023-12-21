acc349c7d92b9e7f1040a8fafd3afc36f2c74367
INSEONG-PC\정인성 <twin2688@naver.com>
2023-12-21 23:47:57 +0900

    메인 페이지 라우터 작업

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Main from "./pages/Main";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Main />}></Route>
           <Route path="/Login" element={<Login />}></Route>
           <Route path="/SignUp" element={<SignUp />}></Route>
         </Routes>
