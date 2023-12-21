2e1aaa356fbac422c38a0bd4abfc11f7e40ca20f
INSEONG-PC\정인성 <twin2688@naver.com>
2023-12-21 23:47:20 +0900

    메인 페이지 작업 중 (스와이퍼)

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+import MainS from "../components/MainS";
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+      <MainS />
+    </div>
+  );
+}
